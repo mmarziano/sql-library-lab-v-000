f2875a9bb26b78a86880ee9e4f1ba8207d6796a1
Monica Marziano <mmarziano1@gmail.com>
2019-01-02 15:07:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT Characters.name, COUNT(Character_books.book_id) as total_appearances FROM characters JOIN character_books ON Characters.id = Character_books.book_id GROUP BY Characters.name ORDER BY total_appearances DESC"
+  "SELECT Characters.name, COUNT(Character_books.book_id) as total_appearances FROM characters JOIN character_books ON Characters.id = Character_books.character_id GROUP BY Characters.name ORDER BY total_appearances DESC"
 end
