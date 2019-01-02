eb57ef5a56c63cb37a851cf9a20659b1fd9af7a8
Monica Marziano <mmarziano1@gmail.com>
2019-01-02 15:06:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title FROM series JOIN characters ON Series.id = Characters.series_id WHERE Characters.species = 'human' GROUP BY Series.title"
+  "SELECT Series.title FROM series JOIN characters ON Series.id = Characters.series_id WHERE Characters.species = 'human' GROUP BY Series.title LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
